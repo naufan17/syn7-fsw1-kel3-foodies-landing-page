4472cac5c4f62675e780fb98cb2264049a216add
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-03-22 00:08:23 +0700

    fix: adjust mobile appearance

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+<<<<<<< HEAD
         primary: "#FA4A0C",
         dark: "#252B42",
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
+=======
         primary: '#FA4A0C',
         dark: '#252B42',
         secondary: '#737373',
+>>>>>>> feat/account
       },
     },
   },
